0e9075843a19d11460460a0151fc56d82b73dff8
chmutenkoa <alenachm@gmail.com>
2022-08-01 18:57:26 +0300

    Sale added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
 
         String[] products = {"Молоко", "Хлеб", "Яблоки", "Сыр"};
         int[] prices = {100, 75, 110, 800};
-        Product food = new Product(products, prices);
+        String[] foodSale = {"Яблоки", "Сыр"};
+        Product food = new Product(products, prices, foodSale);
         food.productBasket();
 
 
